dbc4abc03979218c14e75a7fe83393ea3b005f9b
Jake Wharton <jw@squareup.com>
2021-03-31 14:41:10 -0400

    Upgrade to Compose beta03
    DefaultMonotonicFrameClock is no more! Technically it's still there, but it's no longer used by the internals such that it's safe to remove our throwing version.

--- compose/src/commonMain/kotlin/androidx/compose/runtime/DefaultMonotonicFrameClock.kt
+++
@@ -1,6 +0,0 @@
-package androidx.compose.runtime
-
-actual val DefaultMonotonicFrameClock: MonotonicFrameClock get() {
-  throw UnsupportedOperationException(
-    "No default MonotonicFrameClock! You must include one in your CoroutineContext.")
-}
