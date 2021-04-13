08b5ea622a01230cd0e63cfa9edd9473a20c91ad
Jake Wharton <jw@squareup.com>
2021-04-13 00:04:25 -0400

    Remove orientation from example and tests
    We will get a more comprehensive example soon enough.

--- example/android/src/main/java/example/android/sunspot/AndroidSunspotBox.kt
+++ example/android/src/main/java/example/android/sunspot/AndroidSunspotBox.kt
@@ -7,9 +7,5 @@ import example.sunspot.widget.SunspotBox
 class AndroidSunspotBox(
   override val value: LinearLayout,
 ) : SunspotBox<View> {
-  override fun orientation(orientation: Boolean) {
-    value.orientation = if (orientation) LinearLayout.VERTICAL else LinearLayout.HORIZONTAL
-  }
-
   override val children = ViewGroupChildren(value)
 }
