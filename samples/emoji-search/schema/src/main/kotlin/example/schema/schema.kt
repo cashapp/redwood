67d11c474a22343e18be2aa1dce7af428e7b5546
Jake Wharton <jw@squareup.com>
2022-08-28 20:35:52 -0400

    Add schema for Emoji Search sample

---
+++ samples/emoji-search/schema/src/main/kotlin/example/schema/schema.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2021 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package example.schema
+
+import app.cash.redwood.schema.Children
+import app.cash.redwood.schema.Property
+import app.cash.redwood.schema.Schema
+import app.cash.redwood.schema.Widget
+
+@Schema(
+  [
+    Column::class,
+    ScrollableColumn::class,
+    TextInput::class,
+    Image::class,
+  ],
+)
+interface EmojiSearch
+
+@Widget(1)
+data class Column(
+  @Children(1) val children: List<Any>,
+)
+
+@Widget(2)
+data class ScrollableColumn(
+  @Children(1) val children: List<Any>,
+)
+
+@Widget(3)
+data class TextInput(
+  @Property(1) val hint: String,
+  @Property(2) val text: String,
+  @Property(3) val onTextChanged: (String) -> Unit,
+)
+
+@Widget(4)
+data class Image(
+  @Property(1) val url: String,
+)
