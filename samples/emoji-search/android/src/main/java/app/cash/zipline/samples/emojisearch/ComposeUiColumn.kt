0e8911b83e977637e4efdb4c803ee834914421b4
colinrtwhite <colin@colinwhite.me>
2022-08-30 16:43:58 -0700

    Center horizontally.

--- samples/emoji-search/android/src/main/java/app/cash/zipline/samples/emojisearch/ComposeUiColumn.kt
+++ samples/emoji-search/android/src/main/java/app/cash/zipline/samples/emojisearch/ComposeUiColumn.kt
@@ -2,6 +2,7 @@ package app.cash.zipline.samples.emojisearch
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import app.cash.redwood.LayoutModifier
 import example.schema.widget.Column
 
@@ -11,7 +12,9 @@ class ComposeUiColumn : Column<@Composable () -> Unit> {
   override val children = ComposeUiWidgetChildren()
 
   override val value = @Composable {
-    Column {
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
       children.render()
     }
   }
