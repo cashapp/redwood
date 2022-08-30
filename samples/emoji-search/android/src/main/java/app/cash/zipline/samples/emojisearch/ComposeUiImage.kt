d97902c231be7387491bfddf866c374f57080c75
colinrtwhite <colin@colinwhite.me>
2022-08-30 16:39:16 -0700

    Match iOS rendering.

--- samples/emoji-search/android/src/main/java/app/cash/zipline/samples/emojisearch/ComposeUiImage.kt
+++ samples/emoji-search/android/src/main/java/app/cash/zipline/samples/emojisearch/ComposeUiImage.kt
@@ -1,11 +1,14 @@
 package app.cash.zipline.samples.emojisearch
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import app.cash.redwood.LayoutModifier
@@ -19,13 +22,20 @@ class ComposeUiImage : Image<@Composable () -> Unit> {
   override var layoutModifiers: LayoutModifier = LayoutModifier
 
   override val value = @Composable {
-    AsyncImage(
-      model = url,
-      contentDescription = label,
-      modifier = Modifier
-        .size(64.dp)
-        .padding(8.dp),
-    )
+    Row {
+      AsyncImage(
+        model = url,
+        contentDescription = null,
+        modifier = Modifier
+          .size(64.dp)
+          .padding(8.dp),
+      )
+      Text(
+        text = label,
+        modifier = Modifier
+          .align(Alignment.CenterVertically)
+      )
+    }
   }
 
   override fun url(url: String) {
