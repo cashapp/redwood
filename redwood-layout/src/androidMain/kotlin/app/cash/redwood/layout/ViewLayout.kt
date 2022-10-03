f5cd56a50028154843f77b752314f6b27709c7f4
colinrtwhite <colin@colinwhite.me>
2022-10-03 08:50:46 -0700

    Imports.

--- redwood-layout/src/androidMain/kotlin/app/cash/redwood/layout/ViewLayout.kt
+++ redwood-layout/src/androidMain/kotlin/app/cash/redwood/layout/ViewLayout.kt
@@ -15,7 +15,6 @@
  */
 package app.cash.redwood.layout
 
-import app.cash.redwood.flexbox.MeasureSpec as RedwoodMeasureSpec
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.View
@@ -25,6 +24,7 @@ import app.cash.redwood.flexbox.AlignItems
 import app.cash.redwood.flexbox.FlexDirection
 import app.cash.redwood.flexbox.FlexboxEngine
 import app.cash.redwood.flexbox.JustifyContent
+import app.cash.redwood.flexbox.MeasureSpec as RedwoodMeasureSpec
 import app.cash.redwood.widget.MutableListChildren
 import app.cash.redwood.widget.Widget
 
