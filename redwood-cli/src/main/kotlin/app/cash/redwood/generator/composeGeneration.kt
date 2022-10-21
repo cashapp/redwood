23574cf6445f6256b34831886a21ff585f3fa11b
Colin White <colin@colinwhite.me>
2022-10-21 01:13:35 -0700

    Generate method references for composable layout modifier setters.

--- redwood-cli/src/main/kotlin/app/cash/redwood/generator/composeGeneration.kt
+++ redwood-cli/src/main/kotlin/app/cash/redwood/generator/composeGeneration.kt
@@ -87,6 +87,7 @@ fun SunspotButton(
   RedwoodComposeNode<SunspotWidgetFactory<*>, SunspotButton<*>>(
     factory = SunspotWidgetFactory<*>::SunspotButton,
     update = {
+      set(layoutModifier, SunspotButton<*>::layoutModifiers::set)
       set(text, SunspotButton<*>::text)
       set(enabled, SunspotButton<*>::enabled)
       set(onClick, SunspotButton<*>::onClick)
@@ -164,7 +165,7 @@ internal fun generateComposable(
           }
 
           val updateLambda = CodeBlock.builder()
-            .add("set(layoutModifier) { layoutModifiers = layoutModifier }\n")
+            .add("set(layoutModifier, %T::layoutModifiers::set)\n", widgetType)
 
           val childrenLambda = CodeBlock.builder()
           for (trait in widget.traits) {
