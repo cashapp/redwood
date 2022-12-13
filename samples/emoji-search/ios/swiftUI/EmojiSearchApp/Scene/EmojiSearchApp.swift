5904d267d353f6a39c1cb556b8f56ddca9caa894
Alex Skorulis <askorulis@squareup.com>
2022-12-13 13:39:54 +1100

    Update copyright

--- samples/emoji-search/ios/swiftUI/EmojiSearchApp/Scene/EmojiSearchApp.swift
+++ samples/emoji-search/ios/swiftUI/EmojiSearchApp/Scene/EmojiSearchApp.swift
@@ -1,14 +1,24 @@
-// Copyright © Square, Inc. All rights reserved.
+/*
+ * Copyright (C) 2022 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import SwiftUI
 import shared
 
 @main
 struct EmojiSearchApp: App {
-
-    init() {
-        
-    }
 
     var body: some Scene {
         WindowGroup {
@@ -17,25 +27,3 @@ struct EmojiSearchApp: App {
     }
     
 }
-
-class EmojiSearchContent : Redwood_treehouseTreehouseViewContent {
-    func get(app: Redwood_treehouseAppService) -> Redwood_treehouseZiplineTreehouseUi {
-        let treehouesUi = (app as! PresentersEmojiSearchPresenter)
-        return treehouesUi.launch()
-    }
-}
-
-class EmojiSearchWidgetSystem : Redwood_treehouseTreehouseViewWidgetSystem {
-
-    func widgetFactory(app: Redwood_treehouseTreehouseApp<Redwood_treehouseAppService>,
-                       json: Kotlinx_serialization_jsonJson,
-                       protocolMismatchHandler: Redwood_protocol_widgetProtocolMismatchHandler
-    ) -> Redwood_protocol_widgetDiffConsumingNodeFactory {
-        return ProtocolEmojiSearchDiffConsumingNodeFactory<Redwood_widgetSwiftUIView>(
-            widgets: SwiftUIEmojiSearchWidgetFactory(treehouseApp: app, widgetSystem: self),
-            json: json,
-            mismatchHandler: protocolMismatchHandler
-        );
-    }
-}
-
