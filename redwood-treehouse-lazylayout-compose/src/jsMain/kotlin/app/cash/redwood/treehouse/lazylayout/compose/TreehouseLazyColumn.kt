c92cb66423781805d299cd6190ce3c0ed60a5d48
Jake Wharton <jw@squareup.com>
2022-12-22 16:28:21 -0500

    LazyColumn extracts widget version from composition

--- redwood-treehouse-lazylayout-compose/src/jsMain/kotlin/app/cash/redwood/treehouse/lazylayout/compose/TreehouseLazyColumn.kt
+++ redwood-treehouse-lazylayout-compose/src/jsMain/kotlin/app/cash/redwood/treehouse/lazylayout/compose/TreehouseLazyColumn.kt
@@ -17,6 +17,7 @@ package app.cash.redwood.treehouse.lazylayout.compose
 
 import androidx.compose.runtime.Composable
 import app.cash.redwood.LayoutScopeMarker
+import app.cash.redwood.compose.LocalWidgetVersion
 import app.cash.redwood.protocol.compose.ProtocolBridge
 import app.cash.redwood.treehouse.TreehouseUi
 import app.cash.redwood.treehouse.ZiplineTreehouseUi
@@ -25,7 +26,8 @@ import app.cash.redwood.treehouse.lazylayout.api.LazyListIntervalContent
 
 @Composable
 public fun ProtocolBridge.LazyColumn(content: LazyListScope.() -> Unit) {
-  val scope = TreehouseLazyListScope(this)
+  val widgetVersion = LocalWidgetVersion.current
+  val scope = TreehouseLazyListScope(this, widgetVersion)
   content(scope)
   LazyColumn(scope.intervals)
 }
@@ -47,17 +49,21 @@ public inline fun <T> LazyListScope.items(
   }
 }
 
-private class TreehouseLazyListScope(private val provider: ProtocolBridge) : LazyListScope {
+private class TreehouseLazyListScope(
+  private val provider: ProtocolBridge,
+  private val widgetVersion: UInt,
+) : LazyListScope {
   val intervals = mutableListOf<LazyListIntervalContent>()
 
   private class Item(
     private val provider: ProtocolBridge,
+    private val widgetVersion: UInt,
     private val content: @Composable (index: Int) -> Unit,
   ) : LazyListIntervalContent.Item {
 
     override fun get(index: Int): ZiplineTreehouseUi {
       val treehouseUi = IndexedTreehouseUi(content, index)
-      return treehouseUi.asZiplineTreehouseUi(provider, widgetVersion = 1u)
+      return treehouseUi.asZiplineTreehouseUi(provider, widgetVersion)
     }
   }
 
@@ -67,7 +73,7 @@ private class TreehouseLazyListScope(private val provider: ProtocolBridge) : Laz
   ) {
     intervals += LazyListIntervalContent(
       count,
-      itemProvider = Item(provider, itemContent),
+      itemProvider = Item(provider, widgetVersion, itemContent),
     )
   }
 }
