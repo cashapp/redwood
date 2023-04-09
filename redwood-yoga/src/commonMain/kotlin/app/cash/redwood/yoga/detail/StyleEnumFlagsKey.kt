1a9e6a9e773c40a7d000dcc00b5ddeeac7527526
Colin White <colin@colinwhite.me>
2023-04-09 02:43:37 -0700

    Formatting.

--- redwood-yoga/src/commonMain/kotlin/app/cash/redwood/yoga/detail/StyleEnumFlagsKey.kt
+++ redwood-yoga/src/commonMain/kotlin/app/cash/redwood/yoga/detail/StyleEnumFlagsKey.kt
@@ -3,6 +3,6 @@ package app.cash.redwood.yoga.detail
 import kotlin.reflect.KClass
 
 data class StyleEnumFlagsKey(
-    val enumClazz: KClass<*>,
-    val index: Int
+  val enumClazz: KClass<*>,
+  val index: Int,
 )
