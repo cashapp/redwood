6dddac94f5f5e7d01a6e4734efec8ed852e67553
Jesse Wilson <jwilson@squareup.com>
2023-04-27 14:43:47 -0400

    Hook up CADisplayLink for a frame pulse on iOS

---
+++ redwood-treehouse-host/src/iosMain/kotlin/app/cash/redwood/treehouse/DisplayLinkTarget.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.redwood.treehouse
+
+import kotlinx.cinterop.ExportObjCClass
+import kotlinx.cinterop.ObjCAction
+import platform.Foundation.NSRunLoop
+import platform.Foundation.NSSelectorFromString
+import platform.QuartzCore.CADisplayLink
+import platform.darwin.NSObject
+
+@ExportObjCClass
+internal class DisplayLinkTarget(
+  private val callback: DisplayLinkTarget.() -> Unit,
+) : NSObject() {
+  private val displayLink: CADisplayLink = CADisplayLink.displayLinkWithTarget(
+    target = this,
+    selector = NSSelectorFromString(this::onFrame.name),
+  )
+
+  /** This function must be public to be a valid candidate for [NSSelectorString]. */
+  @ObjCAction
+  public fun onFrame() {
+    callback(this)
+  }
+
+  fun subscribe() {
+    displayLink.addToRunLoop(
+      NSRunLoop.currentRunLoop,
+      NSRunLoop.currentRunLoop.currentMode,
+    )
+  }
+
+  fun unsubscribe() {
+    displayLink.removeFromRunLoop(
+      NSRunLoop.currentRunLoop,
+      NSRunLoop.currentRunLoop.currentMode,
+    )
+  }
+}
---
+++ redwood-treehouse-host/src/iosMain/kotlin/app/cash/redwood/treehouse/IosDisplayLinkClock.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2023 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.redwood.treehouse
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+public class IosDisplayLinkClock : FrameClock {
+  private lateinit var scope: CoroutineScope
+  private lateinit var dispatchers: TreehouseDispatchers
+  private lateinit var displayLinkTarget: DisplayLinkTarget
+
+  /** Non-null if we're expecting a call to [tickClock]. */
+  private var appLifecycle: AppLifecycle? = null
+
+  override fun start(scope: CoroutineScope, dispatchers: TreehouseDispatchers) {
+    this.scope = scope
+    this.dispatchers = dispatchers
+    this.displayLinkTarget = DisplayLinkTarget {
+      unsubscribe()
+      scope.launch(dispatchers.zipline) {
+        appLifecycle?.sendFrame(0L)
+        appLifecycle = null
+      }
+    }
+  }
+
+  override fun requestFrame(appLifecycle: AppLifecycle) {
+    this.appLifecycle = appLifecycle
+    this.displayLinkTarget.subscribe()
+  }
+}
