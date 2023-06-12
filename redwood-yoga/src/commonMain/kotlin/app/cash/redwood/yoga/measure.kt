cb293e0fd1a333e6386d9781661868f34b241d98
Colin White <colin@colinwhite.me>
2023-06-12 03:09:01 -0700

    Add public API bindings for redwood-yoga. (#1184)
    * Import a Kotlin common version of Yoga.
    
    * Add public API bindings for redwood-yoga.

---
+++ redwood-yoga/src/commonMain/kotlin/app/cash/redwood/yoga/measure.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2023 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.redwood.yoga
+
+import app.cash.redwood.yoga.internal.Yoga
+import kotlin.jvm.JvmInline
+
+public fun interface MeasureCallback {
+  public fun measure(
+    node: Node,
+    width: Float,
+    widthMode: MeasureMode,
+    height: Float,
+    heightMode: MeasureMode,
+  ): Size
+}
+
+public data class Size(
+  val width: Float,
+  val height: Float,
+) {
+  public companion object {
+    public const val Undefined: Float = Yoga.YGUndefined
+  }
+}
+
+@JvmInline
+public value class MeasureMode private constructor(private val ordinal: Int) {
+
+  override fun toString(): String = when (ordinal) {
+    0 -> "Undefined"
+    1 -> "Exactly"
+    2 -> "AtMost"
+    else -> throw AssertionError()
+  }
+
+  public companion object {
+    public val Undefined: MeasureMode = MeasureMode(0)
+    public val Exactly: MeasureMode = MeasureMode(1)
+    public val AtMost: MeasureMode = MeasureMode(2)
+  }
+}
