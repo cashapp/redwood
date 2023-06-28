fc8c4ab68913b9dd12c3f618162c228ee22433b7
Veyndan Stuart <veyndan@gmail.com>
2023-06-28 14:42:07 +0100

    Fill width in `align-self` screenshot tests

--- redwood-layout-shared-test/src/main/kotlin/app/cash/redwood/layout/AbstractFlexContainerTest.kt
+++ redwood-layout-shared-test/src/main/kotlin/app/cash/redwood/layout/AbstractFlexContainerTest.kt
@@ -82,6 +82,7 @@ abstract class AbstractFlexContainerTest<T : Any> {
     assumeTrue(flexDirectionEnum in listOf(FlexDirectionEnum.Row, FlexDirectionEnum.Column))
     val flexDirection = flexDirectionEnum.toFlexDirection()
     val container = flexContainer(flexDirection)
+    container.width(Constraint.Fill)
     container.margin(Margin(start = 5.dp, end = 10.dp, top = 20.dp, bottom = 20.dp))
     movies.forEachIndexed { index, movie ->
       val modifier = when (index % 4) {
