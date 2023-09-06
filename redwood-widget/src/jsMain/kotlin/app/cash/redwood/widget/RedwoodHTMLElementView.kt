64c9498be538e0f79adca17a92bbb61ce4a994fb
Jake Wharton <jw@squareup.com>
2023-09-06 14:22:59 -0400

    Add RedwoodView which wraps HTMLElement (#1453)

---
+++ redwood-widget/src/jsMain/kotlin/app/cash/redwood/widget/RedwoodHTMLElementView.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2023 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.redwood.widget
+
+import app.cash.redwood.ui.Size
+import app.cash.redwood.ui.UiConfiguration
+import app.cash.redwood.ui.dp
+import app.cash.redwood.widget.Widget.Children
+import kotlinx.browser.window
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import org.w3c.dom.HTMLElement
+import org.w3c.dom.MediaQueryList
+
+public fun HTMLElement.asRedwoodView(): RedwoodView<HTMLElement> {
+  checkNotNull(parentNode) {
+    "Element $this must be attached to Document to be used as RedwoodView"
+  }
+  return RedwoodHTMLElementView(this)
+}
+
+private class RedwoodHTMLElementView(
+  element: HTMLElement,
+) : RedwoodView<HTMLElement> {
+  override val children: Children<HTMLElement> = HTMLElementChildren(element)
+  override val uiConfiguration = MutableStateFlow(
+    UiConfiguration(
+      darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches,
+      viewportSize = Size(width = element.offsetWidth.dp, height = element.offsetHeight.dp),
+    ),
+  )
+
+  init {
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", { event ->
+      uiConfiguration.update { old ->
+        UiConfiguration(
+          darkMode = event.unsafeCast<MediaQueryList>().matches,
+          safeAreaInsets = old.safeAreaInsets,
+          viewportSize = old.viewportSize,
+          density = old.density,
+        )
+      }
+    })
+
+    // TODO Watch density change
+    //  https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio#javascript_2
+
+    // TODO Watch size change
+    //   https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver
+  }
+}
