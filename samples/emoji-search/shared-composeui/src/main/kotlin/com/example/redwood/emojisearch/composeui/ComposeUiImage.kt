b7ba297f091261d635dbbcda769f38ed633ca15f
Jake Wharton <jw@squareup.com>
2024-02-22 15:59:40 -0500

    Spotless

--- samples/emoji-search/shared-composeui/src/main/kotlin/com/example/redwood/emojisearch/composeui/ComposeUiImage.kt
+++ samples/emoji-search/shared-composeui/src/main/kotlin/com/example/redwood/emojisearch/composeui/ComposeUiImage.kt
@@ -15,7 +15,6 @@
  */
 package com.example.redwood.emojisearch.composeui
 
-import app.cash.redwood.Modifier as RedwoodModifier
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
@@ -24,6 +23,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import app.cash.redwood.Modifier as RedwoodModifier
 import coil3.compose.AsyncImage
 import com.example.redwood.emojisearch.widget.Image
 
