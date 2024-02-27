5bbbeb86c5048da9fb025fb4b6789a8f57aa2b72
Colin White <colin@colinwhite.me>
2024-02-27 10:21:05 -0800

    Fix blocking touch events to non-subviews below Row/Column/Box on iOS. (#1823)
    * Fix blocking touch events to non-subviews below Row/Column/Box on iOS.
    
    * Use collection method.

--- build-support/src/main/kotlin/app/cash/redwood/buildsupport/RedwoodBuildPlugin.kt
+++ build-support/src/main/kotlin/app/cash/redwood/buildsupport/RedwoodBuildPlugin.kt
@@ -51,7 +51,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 private const val REDWOOD_GROUP_ID = "app.cash.redwood"
 
 // HEY! If you change the major version update release.yaml doc folder.
-private const val REDWOOD_VERSION = "0.10.0-SNAPSHOT"
+private const val REDWOOD_VERSION = "0.10.0-alpha.square.4"
 
 @Suppress("unused") // Invoked reflectively by Gradle.
 class RedwoodBuildPlugin : Plugin<Project> {
