d3b544d528b07edef5afdb5300d33d301c2950ff
Jake Wharton <jw@squareup.com>
2024-06-19 12:04:54 -0400

    Remove code supporting old memory model (#2125)

--- test-app/ios-uikit/TestApp/IosHostApi.swift
+++ test-app/ios-uikit/TestApp/IosHostApi.swift
@@ -27,15 +27,9 @@ class IosHostApi : HostApi {
             request.addValue(value, forHTTPHeaderField: name)
         }
         let task = client.dataTask(with: request) { data, response, error in
-            // The KMM memory model doesn't do shared objects well, so Zipline expects the callback
-            // on the same thread that the download was initiated from. This happens to be the main
-            // thread, so we can bounce back to that thread for now.
-            // Switching to the new KMM memory model may remove the need for this.
-            DispatchQueue.main.async {
-                completionHandler(data.map {
-                    return String(decoding: $0, as: UTF8.self)
-                }, error)
-            }
+            completionHandler(data.map {
+                return String(decoding: $0, as: UTF8.self)
+            }, error)
         }
 
         task.resume()
