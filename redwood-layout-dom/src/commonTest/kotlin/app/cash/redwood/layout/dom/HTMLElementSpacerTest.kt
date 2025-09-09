84dc041e0f4fd6b1a6b134a7910970fe3fe1bde1
Jesse Wilson <jwilson@squareup.com>
2025-09-09 11:22:37 -0400

    Make a real DOM snapshot test

---
+++ redwood-layout-dom/src/commonTest/kotlin/app/cash/redwood/layout/dom/HTMLElementSpacerTest.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2025 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.redwood.layout.dom
+
+import app.cash.redwood.dom.testing.Frame
+import app.cash.redwood.layout.AbstractSpacerTest
+import app.cash.redwood.layout.widget.Spacer
+import app.cash.redwood.snapshot.testing.HTMLElementSnapshotter
+import app.cash.redwood.widget.Widget
+import kotlinx.browser.document
+import kotlinx.dom.appendText
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLElement
+
+class HTMLElementSpacerTest : AbstractSpacerTest<HTMLElement>() {
+
+  override val snapshotterFactory = HTMLElementSnapshotter.Factory(Frame.None)
+
+  override fun widget(): Spacer<HTMLElement> =
+    HTMLElementRedwoodLayoutWidgetFactory(document).Spacer()
+
+  override fun wrap(widget: Widget<HTMLElement>, horizontal: Boolean): HTMLElement {
+    return (document.createElement("div") as HTMLDivElement)
+      .apply {
+        style.display = "flex"
+        style.flexDirection = when {
+          horizontal -> "row"
+          else -> "column"
+        }
+        appendText("Text 1")
+        append(widget.value)
+        appendText("Text 2")
+      }
+  }
+}
---
+++ redwood-snapshot-testing/src/jsMain/kotlin/app/cash/redwood/snapshot/testing/HTMLElementSnapshotter.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2025 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package app.cash.redwood.snapshot.testing
+
+import app.cash.burst.coroutines.CoroutineTestFunction
+import app.cash.redwood.dom.testing.DomSnapshotter
+import app.cash.redwood.dom.testing.Frame
+import org.w3c.dom.HTMLElement
+
+class HTMLElementSnapshotter(
+  private val testFunction: CoroutineTestFunction,
+  private val frame: Frame,
+  private val widget: HTMLElement,
+) : Snapshotter {
+  override suspend fun snapshot(name: String?, scrolling: Boolean) {
+    val domSnapshotter = DomSnapshotter(
+      path = buildString {
+        if (testFunction.packageName != "") {
+          append(testFunction.packageName)
+          append("/")
+        }
+        append(testFunction.className)
+      },
+    )
+    domSnapshotter.snapshot(
+      element = widget,
+      name = buildString {
+        append(testFunction.functionName)
+        if (name != null) {
+          append("_")
+          append(name)
+        }
+      },
+      frame = frame,
+      scrolling = scrolling,
+    )
+  }
+
+  class Factory(
+    val frame: Frame,
+  ) : Snapshotter.Factory<HTMLElement> {
+    private var testFunction: CoroutineTestFunction? = null
+
+    override fun invoke(widget: HTMLElement): Snapshotter {
+      val testFunction = testFunction
+
+      return HTMLElementSnapshotter(
+        testFunction = testFunction ?: error("unexpected invoke() without running test"),
+        frame = frame,
+        widget = widget,
+      )
+    }
+
+    override suspend fun intercept(testFunction: CoroutineTestFunction) {
+      if (this.testFunction != null) {
+        testFunction()
+        return
+      }
+
+      this.testFunction = testFunction
+      try {
+        testFunction()
+      } finally {
+        this.testFunction = null
+      }
+    }
+  }
+}
